71de9f56c03bf71e8bf2901bf9f33ecf5458198d
wuxianguo <wuxianguo@MacBookPro.lan>
2025-06-27 09:24:56 +0800

    print

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ from wxcloudrun import app
 if __name__ == '__main__':
     host = sys.argv[1] if len(sys.argv) > 1 else "0.0.0.0"
     port = int(sys.argv[2]) if len(sys.argv) > 2 else 80
+    print(f"Starting server on {host}:{port}")
     app.run(host=host, port=port)
